a0e421412df6fddc730b71ec0e7147bdd725c06b
pankaj <pankajgupta20dec@gmail.com>
2016-09-01 06:02:42 -0700

    sliceup

--- js/custom.js
+++ js/custom.js
@@ -3,11 +3,28 @@ var wheight,smoke;
 		sizes: function(){
 			wheight = $(window).height();
 			$('.wheight').css({'height': wheight});
+		},
+		accordion: function(){
+			$('.question-answer h6').click(function(){
+				$('.question-answer h6').parent().find('.answer').removeClass('active').slideUp(500);
+				$('.question-answer h6').find('.plus').removeClass('minus');
+				if($(this).parent().find('.answer').hasClass('active')){
+					$(this).parent().find('.answer').removeClass('active').slideUp(500);
+					$(this).find('.plus').removeClass('minus');
+				}else{
+					$(this).parent().find('.answer').addClass('active').slideDown(500);
+					$(this).find('.plus').addClass('minus');
+				}
+			});
 		}
 	}
 	$(document).ready(function(){
 		smoke.sizes();
-		$(".gallery a[rel^='prettyPhoto']").prettyPhoto({animation_speed:'normal',theme:'light_square',slideshow:400, autoplay_slideshow: false});
+		smoke.accordion();
+		if($('.gallery').length){
+			$(".gallery a[rel^='prettyPhoto']").prettyPhoto({animation_speed:'normal',theme:'light_square',slideshow:400, autoplay_slideshow: false});
+		}
+		
 	});
 	$(window).on("load resize",function(e){
 		smoke.sizes();
